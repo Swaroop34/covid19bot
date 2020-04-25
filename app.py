a26a682b1b80846add774b7fb4c324807f44edd3
Swaroop34 <62827061+Swaroop34@users.noreply.github.com>
2020-04-25 13:34:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def processRequest(req):
 
 
 if __name__ == '__main__':
-     port = int(os.getenv('PORT', 8000))
+     port = int(os.getenv('PORT', 5000))
      print("Starting app on port %d" % port)
      app.run(debug=False, port=port, host='0.0.0.0')
     # serve(app, host='0.0.0.0', port=5000)
